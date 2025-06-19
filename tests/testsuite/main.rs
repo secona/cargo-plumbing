b24e44b739852c95f0ed7ee77c676adef6b4a8f8
secona <secona00@gmail.com>
2025-06-19 23:42:45 +0700

    test: Set up functional tests

--- tests/testsuite/main.rs
+++ tests/testsuite/main.rs
@@ -1,1 +1,22 @@
 automod::dir!("tests/testsuite");
+
+use cargo_test_support::{execs, process, ArgLineCommandExt, Execs, Project};
+
+pub fn cargo_plumbing_exe() -> std::path::PathBuf {
+    snapbox::cmd::cargo_bin("cargo-plumbing")
+}
+
+pub trait ProjectExt {
+    fn cargo_plumbing(&self, cmd: &str) -> Execs;
+}
+
+impl ProjectExt for Project {
+    fn cargo_plumbing(&self, cmd: &str) -> Execs {
+        let cargo_plumbing = cargo_plumbing_exe();
+
+        let mut p = process(&cargo_plumbing);
+        p.cwd(self.root()).arg_line(cmd);
+
+        execs().with_process_builder(p)
+    }
+}
