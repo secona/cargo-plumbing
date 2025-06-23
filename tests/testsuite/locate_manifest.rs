9120b1b636a25cd5d0d1c55f313ab45814da4f16
secona <secona00@gmail.com>
2025-06-23 22:31:50 +0700

    test(locate-manifest): Basic functional test

---
+++ tests/testsuite/locate_manifest.rs
@@ -0,0 +1,110 @@
+use cargo_test_support::prelude::*;
+use cargo_test_support::project;
+use cargo_test_support::str;
+
+use crate::ProjectExt;
+
+#[cargo_test]
+fn simple() {
+    let p = project().build();
+
+    p.cargo_plumbing("plumbing locate-manifest")
+        .with_stderr_data(str![[r#"
+[ERROR] unrecognized subcommand 'locate-manifest'
+
+Usage: cargo plumbing <COMMAND>
+
+For more information, try '--help'.
+
+"#]])
+        .with_stdout_data("")
+        .with_status(2)
+        .run();
+}
+
+#[cargo_test]
+fn manifest_path_arg() {
+    let p = project().file("src/lib.rs", "").build();
+
+    let wd = p.root().join("src");
+    let wd = wd.to_str().unwrap();
+
+    p.cargo_plumbing("plumbing locate-manifest")
+        .args(&["--manifest-path", wd])
+        .with_stderr_data(str![[r#"
+[ERROR] unrecognized subcommand 'locate-manifest'
+
+Usage: cargo plumbing <COMMAND>
+
+For more information, try '--help'.
+
+"#]])
+        .with_stdout_data("")
+        .with_status(2)
+        .run();
+}
+
+#[cargo_test]
+fn found_virtual_manifest() {
+    let p = project()
+        .file(
+            "Cargo.toml",
+            r#"
+                [workspace]
+                resolver = "3"
+                members = ["crate1", "crate2"]
+            "#,
+        )
+        .file("crate1/src/lib.rs", "")
+        .file(
+            "crate1/Cargo.toml",
+            r#"
+                [package]
+                name = "crate1"
+                version = "0.1.0"
+                edition = "2024"
+            "#,
+        )
+        .file("crate2/src/lib.rs", "")
+        .file(
+            "crate2/Cargo.toml",
+            r#"
+                [package]
+                name = "crate2"
+                version = "0.1.0"
+                edition = "2024"
+            "#,
+        )
+        .build();
+
+    p.cargo_plumbing("plumbing locate-manifest")
+        .with_stderr_data(str![[r#"
+[ERROR] unrecognized subcommand 'locate-manifest'
+
+Usage: cargo plumbing <COMMAND>
+
+For more information, try '--help'.
+
+"#]])
+        .with_stdout_data("")
+        .with_status(2)
+        .run();
+}
+
+#[cargo_test]
+fn no_manifest_found() {
+    let p = project().no_manifest().build();
+
+    p.cargo_plumbing("plumbing locate-manifest")
+        .with_stderr_data(str![[r#"
+[ERROR] unrecognized subcommand 'locate-manifest'
+
+Usage: cargo plumbing <COMMAND>
+
+For more information, try '--help'.
+
+"#]])
+        .with_stdout_data("")
+        .with_status(2)
+        .run();
+}
