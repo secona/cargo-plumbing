89d59211414c4b8b4ba1c651fdc8796084b649a2
secona <secona00@gmail.com>
2025-07-08 20:41:05 +0700

    feat(tests): Add `cargo_global` function
    fix(tests): Use `cargo_bin!` instead

--- tests/testsuite/main.rs
+++ tests/testsuite/main.rs
@@ -3,14 +3,18 @@ mod cargo_plumbing_read_manifest;
 mod locate_manifest;
 mod read_manifest;
 
-use cargo_test_support::{execs, process, ArgLineCommandExt, Execs, Project};
+use cargo_test_support::{execs, process, ArgLineCommandExt, Execs, Project, TestEnvCommandExt};
+use cargo_util::ProcessBuilder;
 
 pub fn cargo_plumbing_exe() -> std::path::PathBuf {
-    snapbox::cmd::cargo_bin("cargo-plumbing")
+    snapbox::cmd::cargo_bin!("cargo-plumbing").to_path_buf()
 }
 
 pub trait ProjectExt {
+    /// Creates an `Execs` instance to run the `cargo-plumbing` binary
     fn cargo_plumbing(&self, cmd: &str) -> Execs;
+    /// Creates an `Execs` instance to run the globally installed `cargo` command
+    fn cargo_global(&self, cmd: &str) -> Execs;
 }
 
 impl ProjectExt for Project {
@@ -19,6 +23,15 @@ impl ProjectExt for Project {
 
         let mut p = process(&cargo_plumbing);
         p.cwd(self.root()).arg_line(cmd);
+
+        execs().with_process_builder(p)
+    }
+
+    fn cargo_global(&self, cmd: &str) -> Execs {
+        let cargo = std::env::var_os("CARGO").unwrap_or("cargo".into());
+
+        let mut p = ProcessBuilder::new(cargo);
+        p.test_env().cwd(self.root()).arg_line(cmd);
 
         execs().with_process_builder(p)
     }
