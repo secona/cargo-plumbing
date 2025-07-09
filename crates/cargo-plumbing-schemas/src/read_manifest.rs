199e0944ebf2277f2dd22ee0974a1b35e3cbf9f1
secona <secona00@gmail.com>
2025-07-09 23:25:57 +0700

    chore(docs): Add docs to `cargo-plumbing-schemas`

--- crates/cargo-plumbing-schemas/src/read_manifest.rs
+++ crates/cargo-plumbing-schemas/src/read_manifest.rs
@@ -1,3 +1,5 @@
+//! Messages used by `cargo plumbing read-manifest` command
+
 use std::{io::Read, marker::PhantomData, path::PathBuf};
 
 use cargo_util_schemas::core::PackageIdSpec;
@@ -6,23 +8,38 @@ use serde::{Deserialize, Serialize};
 
 use crate::MessageIter;
 
+/// Represents the messages outputted by the `cargo-plumbing read-manifest` command.
+///
+/// This enum captures all possible JSON objects that the command can emit. The `reason`
+/// field acts as a discriminant to distinguish between different message types.
 #[derive(Serialize, Deserialize)]
 #[serde(tag = "reason", rename_all = "kebab-case")]
 #[cfg_attr(feature = "unstable-schema", derive(schemars::JsonSchema))]
 pub enum ReadManifestMessage {
+    /// A message containing the contents of a parsed `Cargo.toml` manifest.
     Manifest {
+        /// The path to the manifest file that was read.
         path: PathBuf,
-        #[cfg_attr(feature = "unstable-schema", schemars(with = "Option<String>"))]
+        /// The package ID specification.
+        ///
+        /// This command also outputs virtual manifests and virtual manifests don't have
+        /// [`PackageIdSpec`], hence the use of [`Option`].
+        #[cfg_attr(
+            feature = "unstable-schema",
+            schemars(with = "Option<String>", description = "The package ID specification")
+        )]
         pkg_id: Option<PackageIdSpec>,
+        /// The fully parsed and deserialized manifest content.
         manifest: TomlManifest,
     },
 }
 
 impl ReadManifestMessage {
+    /// Creates an iterator to parse a stream of [`ReadManifestMessage`]s.
     pub fn parse_stream<R: Read>(input: R) -> MessageIter<R, Self> {
         MessageIter {
-            _m: PhantomData::<Self>,
             input,
+            _m: PhantomData::<Self>,
         }
     }
 }
