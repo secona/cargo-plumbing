0f632c5fbf1bb2d54b44c879db6b67204893d72a
secona <secona00@gmail.com>
2025-08-13 23:40:43 +0700

    docs(schemas): Specify order for locked package

--- crates/cargo-plumbing-schemas/src/write_lockfile.rs
+++ crates/cargo-plumbing-schemas/src/write_lockfile.rs
@@ -16,6 +16,11 @@ pub enum WriteLockfileIn {
     Lockfile {
         version: Option<u32>,
     },
+    /// The locked package from the lockfile
+    ///
+    /// Expected to be inputted in a lexicographical order based on the package
+    /// name, matching the order of the `[[package]]` entries in a `Cargo.lock`
+    /// file.
     LockedPackage {
         #[serde(flatten)]
         package: NormalizedDependency,
